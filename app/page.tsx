1732d328eafa921b535c21a7c26d948ab130df37
Lucialco <cesarmorcilloretuerce@gmail.com>
2026-01-02 21:36:21 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 40, fontFamily: "Arial" }}>
+      <h1>Lucialco — Orlas</h1>
+      <p>Proyecto creado correctamente. Seguimos con la configuración.</p>
+    </main>
+  );
+}
