07d20a1a27b745a94310fc9408c028633fe23d9a
Lucialco <cesarmorcilloretuerce@gmail.com>
2026-01-06 19:09:31 +0100

    Create HomeClient.tsx

---
+++ app/HomeClient.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+export default function HomeClient() {
+  const sp = useSearchParams();
+  // ejemplo: const foo = sp.get("foo");
+
+  return (
+    <>
+      {/* TU JSX */}
+    </>
+  );
+}
