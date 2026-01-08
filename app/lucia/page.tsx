c32ae0595ff99ad56721675a6d96d3515b28ce8b
Lucialco <cesarmorcilloretuerce@gmail.com>
2026-01-08 08:48:46 +0100

    Create page.tsx

---
+++ app/lucia/page.tsx
@@ -0,0 +1,10 @@
+import LuciaClient from "./LuciaClient";
+
+export const metadata = {
+  title: "Lucía · Lucialco Orlas",
+  description: "Conoce a Lucía: la persona detrás de Lucialco Orlas.",
+};
+
+export default function LuciaPage() {
+  return <LuciaClient />;
+}
